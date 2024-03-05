e05f58a1738d8ffdd813efc1021d779a282d2732
Roman Serafymovych <serafymovychr@gmail.com>
2024-03-06 00:12:19 +0300

    ready

---
+++ tree_functions.py
@@ -0,0 +1,116 @@
+# Tree realisation
+
+class Node:
+    def __init__(self, key):
+        self.left = None
+        self.right = None
+        self.val = key
+
+    def __str__(self, level=0, prefix="Root: "):
+        ret = "\t" * level + prefix + str(self.val) + "\n"
+        if self.left:
+            ret += self.left.__str__(level + 1, "L--- ")
+        if self.right:
+            ret += self.right.__str__(level + 1, "R--- ")
+        return ret
+
+def insert(root, key):
+    if root is None:
+        return Node(key)
+    else:
+        if key < root.val:
+            root.left = insert(root.left, key)
+        else:
+            root.right = insert(root.right, key)
+    return root
+
+def search(root, key):
+    if root is None or root.val == key:
+        return root
+    if key < root.val:
+        return search(root.left, key)
+    return search(root.right, key)
+
+def min_value_node(node):
+    current = node
+    while current.left:
+        current = current.left
+    return current
+
+def delete(root, key):
+    if not root:
+        return root
+
+    if key < root.val:
+        root.left = delete(root.left, key)
+    elif key > root.val:
+        root.right = delete(root.right, key)
+    else:
+        if not root.left:
+            temp = root.right
+            root = None
+            return temp
+        elif not root.right:
+            temp = root.left
+            root = None
+            return temp
+        root.val = min_value_node(root.right).val
+        root.right = delete(root.right, root.val)
+    return root
+
+
+def max_traversal(root):
+
+    while root:
+        
+        max = root.val
+        root = root.right
+
+    return max
+
+
+def min_traversal(root):
+
+    while root:
+        
+        min = root.val
+        root = root.left
+
+    return min    
+
+def sum_traversal(root):
+    if root is None:
+        return 0
+        
+    left_sum = sum_traversal(root.left)
+    right_sum = sum_traversal(root.right)
+
+    total_sum = root.val + left_sum + right_sum
+
+    return total_sum
+
+
+# Tree creation
+root = Node(5)
+root = insert(root, 3)
+root = insert(root, 2)
+root = insert(root, 4)
+root = insert(root, 7)
+root = insert(root, 6)
+root = insert(root, 8)
+
+# Output of functions
+
+print('Tree structure')
+print(root)
+
+print('Max element')
+
+print(max_traversal(root))
+
+print('Min element')
+
+print(min_traversal(root))
+
+print('Sum of all elements')
+print(sum_traversal(root))
